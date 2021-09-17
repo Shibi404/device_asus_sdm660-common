46eee01af855b001987c38ac98e6f6e6b06cfca8
Edwin Moquete <edwinmmoquete@gmail.com>
2021-09-17 18:23:59 -0400

    sdm660-common: Build QTI thermal HAL
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -400,6 +400,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.asus_sdm660.qti
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
